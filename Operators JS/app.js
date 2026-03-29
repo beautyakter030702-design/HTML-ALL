8a24adbdf9fdc0a9cdbde5ee809a8f309b65ca3e
beautyhasan34 <hasanbeauty57@gmail.com>
2026-03-29 09:37:20 -0700

    first commit

---
+++ Operators JS/app.js
@@ -0,0 +1,102 @@
+//javacript
+
+/*
+*Arithmetic Operatore
+*/
+/*
+let a = 50;
+let b = 40;
+let c = a + b;
+console.log(c);
+
+
+let d = 40;
+let e = 6;
+let f = d * e;
+console.log(f);
+
+let g = 45;
+let h = 5
+let i = g % h;
+console.log(i);
+
+
+
+let uesrInput = prompt('Enter a Number');
+let number = parseInt(uesrInput);
+
+if (number % 2 === 0) {
+    console.log('${number}is even number');
+}
+else {
+    console.log('${number}is odd number');
+}
+
+
+let x = 10;
+x = x + 1;
+x++; //incrment
+console.log('incrment${x}');
+
+
+let y = 10;
+y = y - 1;
+y--; decrement
+console.log('Decrement${y}');
+*/
+
+
+/*
+*Comparison Operatore
+*/
+
+let p = 5;
+let q = 20
+
+console.log(p !== q);
+console.log(p === q);
+console.log(p == q);
+console.log(p >= q);
+console.log(p <= q);
+console.log(p > q);
+console.log(p < q);
+console.log(p === q);
+
+
+/*
+*Logical Operator
+*/
+
+// && AND Operatore
+// || OR Operatore
+// !(p>q) NOT Operatore
+
+let s = 10;
+let k = 20;
+j = j + k;
+console.log(j);
+
+let j=10;
+let k=20;
+j=j*k;
+j*=k;
+console.log(j);
+
+
+
+
+
+/*
+* Conditional Operatore
+*/
+
+
+
+let t = a < b ? 'True' : 'False';
+console.log(t);
+
+if (a > b) console.log('Ture');
+else console.log('False');
+
+
+
